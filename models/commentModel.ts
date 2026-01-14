dcd98b53c8b479a92dfb6dc7ca78e3d2b218a9b0
yaeluri <yaeli.uri@gmail.com>
2026-01-14 21:28:38 +0200

    handle comment

---
+++ models/commentModel.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  sender: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model("comment", commentSchema);
